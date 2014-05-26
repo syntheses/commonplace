2509053c8b0d05b9d212d12c7863394c0cac8f55
Max <max@teamatb.com>
2014-05-26 08:19:25 -0400

    Add posts controller

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,12 @@ var moment = require('moment');
 
 var fileParser = require('../middleware/fileParser');
 
-var config = require('../config.json');
-var jekyllPath = config.jekyllPath || '.';
-
-var postsPath = jekyllPath + '/_posts/';
-
-var templates = require('../output')();
-
+var posts = require('./posts');
 
 module.exports = function(app) {
-  app.get('/posts', function(req, res) {
-    console.log('posts');
-  })
+  app.get('/posts', posts.index);
 
-  app.post('/posts', fileParser, function(req, res) {
+  app.post('/posts', fileParser, posts.new);
 
-    req.body.date = new Date();
-    var post = templates[req.body.type](req.body);
-
-    fs.writeFile(postsPath + moment(new Date()).format('YYYY-MM-DD') + '-' + slug(req.body.title) + '.md', post, function(err) {
-      if (err)
-        console.log(err);
-      else {
-        console.log('post made')
-        cp.exec('jekyll build', {cwd: jekyllPath}, function(error, stdout, stderr) {
-          res.send({posted: true});
-        });
-      }
-    });
-
-  });
-
-  app.get('/posts/:title', function(req, res) {});
+  app.get('/posts/:title', posts.show);
 };
